6494d925c0e8dcbd6e8ef82010491e6169139e8c
Lemon <516330797@qq.com>
2020-08-17 11:08:03 +0800

    初始化DOM库

---
+++ src/dom.js
@@ -0,0 +1,17 @@
+window.dom = {
+  create(string) {
+    const container = document.createElement("template");
+    container.innerHTML = string.trim();
+    return container.content.firstChild;
+  },
+  after(node, node2) {
+    node.parentNode.insertBefore(node2, node.nextSibling);
+  },
+  before(node, node2) {
+    node.parentNode.insertBefore(node2, node);
+  },
+  append(parent, node) {
+    parent.appendChild(node);
+  },
+  wrap(node, parent) {},
+};
